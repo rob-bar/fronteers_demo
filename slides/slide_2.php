5b94503f1c5d68422e2ef28d3ac3f2ed5de4b35b
Robbie Bardijn <robbie.bardijn@gmail.com>
2013-02-04 10:16:30 +0100

    removed bad slides

--- slides/slide_2.php
+++
@@ -1,4 +0,0 @@
-<section data-state="blackout">
-	<h2>Language Usage</h2>
-
-</section>
--- slides/slide_8.php
+++
@@ -1,8 +0,0 @@
-<section>
-	<h2>Emails have 1 view</h2>
-<!-- 	<pre>
-		<code contenteditable style="font-size: 18px;">
-		</code>
-	</pre> -->
-	<!-- <p></p> -->
-</section>
